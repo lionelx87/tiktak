f0214737daea4e43b6fa8b9c3488f14eee552d1c
Lionel Acosta <lionelx87@gmail.com>
2023-06-26 20:56:19 -0300

    feat(provider): create local video model

---
+++ lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,38 @@
+import 'package:tiktak/domain/entities/video_post.dart';
+
+class LocalVideoModel {
+  final String name;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  LocalVideoModel({
+    required this.name,
+    required this.videoUrl,
+    this.likes = 0,
+    this.views = 0
+  });
+
+  factory LocalVideoModel.fromJsonMap(Map<String, dynamic> json) => LocalVideoModel(
+    name: json['name'],
+    videoUrl: json['videoUrl'],
+    likes: json['likes'] ?? 0,
+    views: json['views'] ?? 0
+  );
+
+  Map<String, dynamic> toJson() => {
+    'caption': name,
+    'videoUrl': videoUrl,
+    'likes': likes,
+    'views': views
+  };
+
+  VideoPost toVideoPostEntity() => VideoPost(
+    caption: name, 
+    videoUrl: videoUrl,
+    likes: likes,
+    view: views,
+  );
+
+
+}
