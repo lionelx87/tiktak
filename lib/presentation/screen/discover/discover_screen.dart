912c149ffac172a90db212915e08513059cb103f
Lionel Acosta <lionelx87@gmail.com>
2023-06-26 23:02:58 -0300

    feat(videos): create page view

--- lib/presentation/screen/discover/discover_screen.dart
+++ lib/presentation/screen/discover/discover_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiktak/presentation/providers/discover_provider.dart';
+import 'package:tiktak/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('DiscoverScreen')),
+    final discoverProvider = context.watch<DiscoverProvider>();
+    // final otroProvider = Provider.of<DiscoverProvider>(context, listen: false);
+
+    return Scaffold(
+      body: discoverProvider.initialLoading ? const Center(
+        child: CircularProgressIndicator( strokeWidth: 2, ),
+      )
+      : VideoScrollableView(videos: discoverProvider.videos)
     );
   }
 }
---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tiktak/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView(
+    {
+      super.key,
+      required this.videos
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.teal,
+        )
+      ],
+    );
+  }
+}
