8f57bc2bf14624b21b9556cc64f9b6f2f9c93395
Lionel Acosta <lionelx87@gmail.com>
2023-06-26 23:29:45 -0300

    feat(app): likes and views

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktak/domain/entities/video_post.dart';
+import 'package:tiktak/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -13,20 +14,37 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.teal,
-        )
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            // VideoPlayer + Gradients
+            // Buttons
+            Positioned(bottom: 40, right: 20, child: VideoButtons(video: videoPost))
+          ],
+        );
+      },
     );
+
+    // return PageView(
+    //   scrollDirection: Axis.vertical,
+    //   physics: const BouncingScrollPhysics(),
+    //   children: [
+    //     Container(
+    //       color: Colors.red,
+    //     ),
+    //     Container(
+    //       color: Colors.blue,
+    //     ),
+    //     Container(
+    //       color: Colors.teal,
+    //     )
+    //   ],
+    // );
   }
 }
+
