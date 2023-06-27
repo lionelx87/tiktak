afb8eff7c7a56a40e450c67c4c13a5d16ab321bb
Lionel Acosta <lionelx87@gmail.com>
2023-06-26 21:18:30 -0300

    feat(app): load videos

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:tiktak/domain/entities/video_post.dart';
+import 'package:tiktak/infrastructure/models/local_video_model.dart';
+import 'package:tiktak/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO: Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // TODO: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts.map( ( video ) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity() ).toList();
+
+    videos.addAll( newVideos );
+
+    initialLoading = false;
     notifyListeners();
 
   }
