21f08f87fc17131f4bfa24051b87158c137afba8
Lionel Acosta <lionelx87@gmail.com>
2023-06-26 23:51:46 -0300

    feat(app): animate_do

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktak/config/helpers/human_formats.dart';
 import 'package:tiktak/domain/entities/video_post.dart';
@@ -17,7 +18,13 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
-        _CustomIconButton(value: video.view, iconData: Icons.remove_red_eye_outlined)
+        const SizedBox(height: 20,),
+        _CustomIconButton(value: video.view, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20,),
+        SpinPerfect(
+          infinite: true, 
+          duration: const Duration(seconds: 5), 
+          child: const _CustomIconButton(value: 0, iconData: Icons.play_circle_outline)),
       ],
     );
   }
@@ -42,7 +49,8 @@ class _CustomIconButton extends StatelessWidget {
       children: [
         IconButton(onPressed: () {}, icon: Icon(iconData, color: color, size: 30,)),
         // Text('$value')
-        Text(HumanFormats.humanReadbleNumber(value.toDouble()))
+        if(value > 0)
+          Text(HumanFormats.humanReadbleNumber(value.toDouble()))
       ],
     );
   }
