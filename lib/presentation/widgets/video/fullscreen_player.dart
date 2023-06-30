b261f46046409a9441b497900b19180833eaf7b7
Lionel Acosta <lionelx87@gmail.com>
2023-06-30 01:46:28 -0300

    feat(video): loading

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
@@ -13,7 +14,39 @@ class FullScreenPlayer extends StatelessWidget {
   );
 
   @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = VideoPlayerController.asset(widget.videoUrl)
+    ..setVolume(0)
+    ..setLooping(true)
+    ..play();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // widget.caption
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshot) {
+        return const Center(
+          child: CircularProgressIndicator(strokeWidth: 2,),
+        );
+      },
+    );
   }
 }
