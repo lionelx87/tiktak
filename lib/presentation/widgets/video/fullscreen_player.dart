9ac1657f241627931a7df18284fed57fb6378047
Lionel Acosta <lionelx87@gmail.com>
2023-07-01 01:30:05 -0300

    feat(video): pause video

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -49,20 +49,29 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
           );
         }
 
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
-              // Gradient
-              // Text
-              Positioned(
-                bottom: 50,
-                left: 20,
-                child: _VideoCaption(caption: widget.caption),
-              )
-            ],
-          )
+        return GestureDetector(
+          onTap: () {
+            if(controller.value.isPlaying) {
+              controller.pause();
+            }else {
+              controller.play();
+            }
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+                // Gradient
+                // Text
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption),
+                )
+              ],
+            )
+          ),
         );
 
 
